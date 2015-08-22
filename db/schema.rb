f8a4f2b04221c942354d9b7ab1fd0dc1a13c39cb
eoris <eoris.work@gmail.com>
2015-08-22 14:31:24 +0300

    creating lists done

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817121929) do
+ActiveRecord::Schema.define(version: 20150822084137) do
 
   create_table "lists", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
