f885ba82fa91c3749de74bde827ca11b19639781
eoris <eoris.work@gmail.com>
2015-08-26 17:10:12 +0300

    temporally added delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,8 @@
 class ProjectsController < ApplicationController
 
   def index
-    @project = Project.new
     @projects = Project.all
+    @project  = Project.new
   end
 
   def show
@@ -19,10 +19,16 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.save
+
     respond_to do |format|
-      format.html { redirect_to @project }
-      format.js
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.js   {}
+        format.json { render json: @project, status: :created, location: @project }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
   end
 
