9d921a92f5c8d8cc2003d05ac12aa8f6522a6d3d
eoris <eoris.work@gmail.com>
2015-08-27 11:48:05 +0300

    add projects are worked

--- app/assets/javascripts/show_new_project_form.js
+++ app/assets/javascripts/show_new_project_form.js
@@ -3,11 +3,5 @@ $(document).ready(function(){
     $('.new_project').css("display", "block");
     return false; 
   });
-
-  // $('.add_new_project').click(function() {
-  // 	$('.new_project').fadeOut(500);
-  // 	return false;
-  // });
-
 })
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,21 +5,9 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
-  def show
-    # @project = Project.find(params[:id])   
-  end
-
-  def new
-    # @project = Project.new    
-  end
-
-  def edit
-    # @project = Project.find(params[:id])
-  end
-
   def create
     @project = Project.new(project_params)
-
+    # byebug
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
@@ -32,19 +20,9 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def update
-    # @project = Project.find(params[:id])
-
-    #   if @project.update(project_params)
-    #     redirect_to @project
-    #   else
-    #     render 'edit'
-    # end    
-  end
-
   def destroy
     @project = Project.find(params[:id])
-    @project.destroy   
+    @project.destroy
     redirect_to @project
   end
 
