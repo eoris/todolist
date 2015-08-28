1cb2842c7b54ff7487600288037f14fcb12bb069
eoris <eoris.work@gmail.com>
2015-08-28 16:13:57 +0300

    delete worked

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
+//show "Add project" form
   $('.button').click(function() {
     $('.new_project').css("display", "block");
     return false; 
   });
 
+//hide project window
   $('.projects').on('click', ".title_trash_list_img", function(){
-  	$(this).parents(".project").fadeOut(500);
+  	$(this).parents(".project").fadeOut(500, function() {
+  		$(this).remove();
+  	});
   });
 })
