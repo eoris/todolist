75cf7772642c23efcb68b54e21c3a90ed9d139e8
eoris <eoris.work@gmail.com>
2015-08-28 17:21:24 +0300

    edit project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,16 +22,6 @@ class ProjectsController < ApplicationController
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
-    # respond_to do |format|
-    #   if @project.destroy
-    #     format.html { redirect_to @project, notice: 'Project was deleted.' }
-    #     format.js   {}
-    #     format.json { render json: @project, status: :created, location: @project }
-    #   else
-    #     format.html { render action: "new" }
-    #     format.json { render json: @project.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
 
