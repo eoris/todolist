aad7735b0adf0d86fb05d32205a0c6557c5f9a69
eoris <eoris.work@gmail.com>
2015-08-28 15:35:01 +0300

    delete worked

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
     $('.new_project').css("display", "block");
     return false; 
   });
-})
 
+  $('.projects').on('click', ".title_trash_list_img", function(){
+  	$(this).parents(".project").fadeOut(500);
+  });
+})
--- app/views/projects/destroy.js
+++ app/views/projects/destroy.js
@@ -1,13 +1,1 @@
-// $(document).ready(function(){
-// 	$('#').fadeOut(500);
-// 	console.log('test')
-// })
-
-
-$(document).ready(function(){
-	$('.title_trash_list_img').click(function() {
-		$('#\\1\\').fadeOut(500);
-		console.log('test');
-	});
-})
 
