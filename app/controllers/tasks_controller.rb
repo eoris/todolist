451b55858bee4767d74a3fe1dde76764c3830fff
eoris <eoris.work@gmail.com>
2015-08-30 23:42:29 +0300

    do tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  def index
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @task = @project.task.create(task_params)
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :date)
+    end
 end
